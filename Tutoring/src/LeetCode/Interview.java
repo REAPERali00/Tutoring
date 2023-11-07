796fa20aa1e40d5fdea487de03c41b069e5f4789
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2023-11-07 17:38:46 -0500

    11-7-2023

--- Tutoring/src/LeetCode/Interview.java
+++ Tutoring/src/LeetCode/Interview.java
@@ -581,5 +581,27 @@ public class Interview {
 	public String makeReadable(int seconds) {
 		return String.format("%02d:%02d:%02d", seconds / 3600, (seconds / 60) % 60, seconds % 60);
 	}
+
+	public String convert(String s, int nRows) {
+	    char[] c = s.toCharArray();
+	    int len = c.length;
+	    StringBuffer[] sb = new StringBuffer[nRows];
+	    for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
+	    
+	    int i = 0;
+	    while (i < len) {
+	        for (int idx = 0; idx < nRows && i < len; idx++) // vertically down
+	            sb[idx].append(c[i++]);
+	        for (int idx = nRows-2; idx >= 1 && i < len; idx--) // obliquely up
+	            sb[idx].append(c[i++]);
+	    }
+	    for (int idx = 1; idx < sb.length; idx++)
+	        sb[0].append(sb[idx]);
+	    return sb[0].toString();
+	}
+
+
+
 }
+
 
--- Tutoring/src/Peer/Yining.java
+++ Tutoring/src/Peer/Yining.java
@@ -28,7 +28,7 @@ public class Yining {
 		System.out.println(test2.add(12));//syso + ctrl + space 
 	}
 	
-	public static void main(String[] args) {
+	public static void secondSession() {
 		Test test= new Test(1,2);
 		Scanner scan = new Scanner(System.in); 
 		double weight;
@@ -39,9 +39,43 @@ public class Yining {
 		System.out.println(test.devide(1, 0));
 		n = 12;
 		weight = scan.nextDouble();
+	}
 	
+	public static String printLetter(String letter) {
+		String result = ""; 
+		switch(letter) {
+		case "A": 
+			result = "80-100";
+			break;
+		case "B": 
+			result = "70-79";
+			break; 
+		case "C": 
+			result = "60-69";
+			break; 
+		case "D": 
+			result = "50-59";
+			break; 
+			default: 
+				result = "Not implemented."; 
+		}
+		return result;
+	}
+	
+	public static void thirdSession(){
+		char letter = 'B'; 
+		System.out.print("You got ");
+		if(letter == 'A' || letter < 'A')
+			System.out.println("80-100");
+		else if (letter >= 'B')
+			System.out.println("70 - 79");
+		String s = "some value"; 
 		
-		
+		System.out.println(printLetter("C"));
+	}
+	
+	public static void main(String[] args) {
+		thirdSession();
 	}
 	
 
