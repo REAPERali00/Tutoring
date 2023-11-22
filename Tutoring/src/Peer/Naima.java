62c34899370ea7043a15ab8315043a9f3b717039
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2023-11-22 10:58:03 -0500

    11/22/2023

--- Tutoring/src/Peer/Naima.java
+++ Tutoring/src/Peer/Naima.java
@@ -2,24 +2,21 @@ package Peer;
 
 import java.util.Scanner;
 
-public class Naima {
+public class Naima implements Starting{
 	
-/**
- * 
- * Topics: 
- * 0. Error types: Sysntax, Compile time, Run time, Logical 
- * 1. Variables: primitive and reference 
- * 2. Methods : code that is written somewhere else, and has a name.A method has a return type and a parameter. 
- * 3. Class, how to use code that is written in a different file. l
- * 4. Class detail: instance variables, getter and setter (access modifiers) , 
- * 		Constructors, other methods inside class. 
- * 
- * 5. Conditional statements
- * 6. loops
- * 7. array 
- */
+	/**
+	 * 
+	 * Topics: 0. Error types: Sysntax, Compile time, Run time, Logical 1.
+	 * Variables: primitive and reference 2. Methods : code that is written
+	 * somewhere else, and has a name.A method has a return type and a parameter. 3.
+	 * Class, how to use code that is written in a different file. l 4. Class
+	 * detail: instance variables, getter and setter (access modifiers) ,
+	 * Constructors, other methods inside class.
+	 * 
+	 * 5. Conditional statements 6. loops 7. array
+	 */
 	
-	public static void firstSession() {
+	public void firstSession() {
 		//Declaration: type + name. 
 		int m = 2;
 		int n;
@@ -61,7 +58,7 @@ public class Naima {
 //		System.out.println(new Test().n);
 	}
 
-	public static void secondSession() {
+	public void secondSession() {
 		Scanner scan = new Scanner(System.in);
 		int age = 0; 
 		System.out.print("Enter your age: ");
@@ -84,23 +81,48 @@ public class Naima {
 		
 	}
 	
-	public static void thirdSession() {
+	public void modify_int(int arr[]) {
+		arr[0] += 4;
+		System.out.println(arr[0]);
+	}
+	
+	/*
+	 * take the array, and change each of its values to start at 1 and 
+	 * end at length. 
+	 * ex: if arr[5], arr = {1,2,3,4,5}; 
+	 */
+	public void inject_arr(int[] arr) {
+		for(int i = 0;i < arr.length; i++) {
+			arr[i] = i+1;
+		}
+	}
+	
+	public void array_info() {
+		int arr[] = new int[5]; //we use this when we don't know the exact elements.
+		String names[] = {"Ali", "Naima"}; //Use this if you do know the elements
+		//Prints from the last element till the first part of the array
+		for(int i = names.length-1; i >= 0 ; i--) {
+			System.out.printf("%d.%s\t", i, names[i]); //print the index and the element
+		}
+		System.out.println(); // Go to the next line 
+		
+		//Pass by reference example: 
+		arr[0] = 2; 
+		modify_int(arr);
+		System.out.println(arr[0]);
+		
+		//Setting all the elements of the array to be i+1
+		inject_arr(arr);
+		
+		//Printing the array. 
+		for(int i = 0; i < arr.length; i++) 
+			System.out.println(arr[i]);
 		
 	}
 	
-	
-	/**
-	 * Make a circle, that has a radius. 
-	 * make the rad, and a mehtod to calc the area. 
-	 * make use of Constructors, getters, setters. 
-	 * toString method. 
-	 * In main method, use scanner to read radius and calculate the area. 
-	 * use to string to print the reslt! 
-	 * 
-	 */
-	public static void main(String[] args) {
-		thirdSession();
-		
+	@Override
+	public void start() {
+		array_info();
 	}
 
 }
