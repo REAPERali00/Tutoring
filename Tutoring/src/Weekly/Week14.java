c948cf3efa823dd4719664f40502bd7f4454cfa5
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2023-12-11 19:26:18 -0500

    12/11/2023

--- Tutoring/src/Weekly/Week14.java
+++ Tutoring/src/Weekly/Week14.java
@@ -3,7 +3,10 @@ package Weekly;
 public class Week14 {
 	
 	public static void friday() {
-		
+		int arr[] = {1,2,3};
+		for(int i = 0; i < arr.length; i--) {
+			arr[i] = 50; 
+		}
 	}
 	
 	public static void main(String[] args) {
