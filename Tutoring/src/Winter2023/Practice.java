922b80337d7897d3e67c2a45eef90f732613b968
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2024-02-04 10:27:51 -0500

    Preperations done

---
+++ Tutoring/src/Winter2023/Practice.java
@@ -0,0 +1,21 @@
+package Winter2023;
+
+/**
+ * For this class we want to practice making the structure of the class:
+ * What is our goal? make a class called Book, the book has a name, author, and
+ * page number.
+ * make a method that prints the books information!
+ * Bonus points: write a method that reader a review of the book form the user!
+ */
+public class Practice {
+    // Make the Variable Declarations:
+
+    // Make the constructor
+
+    // Make the getter/setter:
+
+    // Make the toString Method:
+
+    // Make the worker method:
+
+}
---
+++ Tutoring/src/Winter2023/Week4.java
@@ -0,0 +1,12 @@
+package Winter2023;
+
+public class Week4 {
+
+    public void sunday() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        new Week4().sunday();
+    }
+}
