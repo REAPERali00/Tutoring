fe5c73fedc1e594a2c465aaebb4f0527de0d270a
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2024-02-10 08:29:30 -0500

    Finished the quiz

--- Tutoring/src/Winter2023/Week5.java
+++ Tutoring/src/Winter2023/Week5.java
@@ -2,7 +2,15 @@ package Winter2023;
 
 public class Week5 {
     public void friday() {
+        System.out.println("Hi my name is \"Alireza\"  ");
+        String abc = "1 2 3 ";
+        int c = 1;
+        System.out.println(abc + abc.length());
+        System.out.println(5D / 2);
+        if (c != 1)
+            System.out.println("hi");
         System.out.println("Hello");
+
     }
 
     public static void main(String[] args) {
