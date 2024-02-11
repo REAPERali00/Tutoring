744a4219b7ed9984aa2e5318944832689c8175d9
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2024-02-11 11:50:11 -0500

    2/11/2024

--- Tutoring/src/Winter2023/Week5.java
+++ Tutoring/src/Winter2023/Week5.java
@@ -13,6 +13,23 @@ public class Week5 {
 
     }
 
+    public static void sunday() {
+        // System.out.println("Hello \"ali\"");
+
+        // String abc = "1 2 3 ";
+        // System.out.println(abc + abc.length());
+        // System.out.println((double) (5 / 2));
+        // System.out.println(5D / 2);
+        // double n = Math.PI;
+        // Scanner input;
+        // System.out.print("Enter a number:");
+        // input.nextInt();
+        // input.close();
+        int n = 1;
+        System.out.printf("%d, %d, %d, %d, %d,", n, --n, n--, n++, ++n);
+
+    }
+
     public static void main(String[] args) {
         new Week5().friday();
     }
