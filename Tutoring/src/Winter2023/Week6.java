c34a99e2130fcb8c5a9e092e3ec818daddba1027
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2024-02-16 16:49:31 +0000

    Minor setup for the friday tutoring

---
+++ Tutoring/src/Winter2023/Week6.java
@@ -0,0 +1,16 @@
+package Winter2023;
+
+public class Week6 {
+    /*
+     * Todays Topic: Github! 
+     * we will be going over how to set up your account, and how to make a repository. 
+     * next, we will go over how to push, pull, add, and 
+     */
+    public void friday() {
+        System.out.println("Lets start Week 6!");
+    }
+
+    public static void main(String[] args) {
+        new Week6().friday();
+    }
+}
