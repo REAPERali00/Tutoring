5d6d84a7c4f82153b68433664502b204923463dc
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2024-02-16 20:15:54 -0500

    2/16/2024

--- Tutoring/src/Winter2023/Week6.java
+++ Tutoring/src/Winter2023/Week6.java
@@ -2,12 +2,13 @@ package Winter2023;
 
 public class Week6 {
     /*
-     * Todays Topic: Github! 
-     * we will be going over how to set up your account, and how to make a repository. 
-     * next, we will go over how to push, pull, add, and 
+     * Todays Topic: Github!
+     * we will be going over how to set up your account, and how to make a
+     * repository.
+     * next, we will go over how to push, pull, add, and
      */
     public void friday() {
-        System.out.println("Lets start Week 6!");
+        System.out.println("Hello world");
     }
 
     public static void main(String[] args) {
