ebfea9bdf81062961aa569ffa7b8d8c555b382ae
Alireza Jadidi <alireza2000jadidi@gmail.com>
2024-02-22 13:08:13 -0500

    Session finsihed

--- PeerTutoring/src/Peer/Lorena.java
+++ PeerTutoring/src/Peer/Lorena.java
@@ -1,6 +1,33 @@
 package Peer;
 
 public class Lorena {
+    public static class Example {
+        // Instance Variables:
+        private int num;
+
+        // Constructor:
+        public Example() {
+            num = 12;
+        }
+
+        // Getters/Setters:
+        public int getNum() {
+            return num;
+        }
+
+        // worker methods:
+        public int add(int num1, int num2) {
+            return num1 + num2;
+        }
+    }
+
     public static void main(String[] args) {
+        System.out.println("");
+        // Primtives and references.
+        int num = 0;
+        // num = 12;
+        System.out.println(num);
+        Example example = new Example();
+        System.out.println(example.add(1, 2));
     }
 }
