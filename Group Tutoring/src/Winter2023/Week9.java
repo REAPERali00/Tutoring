001f5ec54be823e6d5a0aa8a6aca158f6958830d
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2024-03-09 12:01:43 -0500

    3/9/2024

--- Group Tutoring/src/Winter2023/Week9.java
+++ Group Tutoring/src/Winter2023/Week9.java
@@ -18,7 +18,33 @@ public class Week9 {
         System.out.println("hello world");
     }
 
+    void saturday() {
+        /*
+         * Logical operations:
+         * == equals
+         * > more then
+         * < less then
+         * >= greater or equal
+         * <= less then or equal
+         * ! not (basically flip boolean)
+         * != not equal
+         * && and (both sides most be true) true && true = true
+         * || or (either one must be true) false || false = false
+         */
+        int age = 13;
+        boolean ageCheck = age >= 13 && age <= 19;
+        if (ageCheck) {
+            System.out.println("Your a teenager");
+        } else if (age >= 19 && age <= 40) {
+            System.out.println("Your an adult");
+        } else {
+            System.out.println("You are elderly");
+        }
+
+        System.out.println(ageCheck);
+    }
+
     public static void main(String[] args) {
-        new Week9().friday();
+        new Week9().saturday();
     }
 }
