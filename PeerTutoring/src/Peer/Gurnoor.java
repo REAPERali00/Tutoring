213013c63622a921ce3ab7538726d8184875f37f
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2024-03-13 14:46:34 -0400

    finsiehd

--- PeerTutoring/src/Peer/Gurnoor.java
+++ PeerTutoring/src/Peer/Gurnoor.java
@@ -1,19 +1,24 @@
 package Peer;
 
+import java.util.Scanner;
+
 public class Gurnoor {
+
+    private static Scanner scan = new Scanner(System.in);
+
     private static class Example {
         private int num;
         private String name;
 
         // Constructors
         public Example() {
-            this(1, "ALI");
+            this(1, "Default Constructor");
             // num = 12;
             // name = "Default name";
         }
 
         public Example(int num) {
-            this(num, "Default name");
+            this(num, "non default constructor with only num");
         }
 
         public Example(int num, String name) {
@@ -52,16 +57,11 @@ public class Gurnoor {
         }
     }
 
-    /**
-     * Conditional statements: if else, booleans, logical opersations
-     * Loops: for, while, do while loop
-     * array: bunch
-     */
-    public static void main(String[] args) {
+    void classExample() {
         int num = 4, num2 = 8;
         Example ex = new Example(1, "Ali");
         Example ex2 = new Example(45);
-        ex2.setName("ALi");
+        ex2.setName("Ali");
         ex2.setNum(12);
 
         System.out.println(ex2);
@@ -70,5 +70,77 @@ public class Gurnoor {
         ex.addition(num, num2);
 
         ex.printAddition(1, num2);
+
+    }
+
+    void printMenu() {
+        System.out.print("choose one of the following: " +
+                "\n1. print hello world"
+                + "\n2. print user name"
+                + "\n3.Exit"
+                + "\nEnter here: ");
+    }
+
+    int getChoice() {
+        printMenu();
+        int choice = 0;
+        return scan.nextInt();
+    }
+
+    void displayUserName() {
+        System.out.print("whats your name? ");
+        System.out.println("Hello " + scan.next());
+    }
+
+    void exeMenu(int choice) {
+        final int printName = 2, printHello = 1;
+        switch (choice) {
+            case printHello:
+                System.out.println("hello world");
+                break;
+            case printName:
+                displayUserName();
+                break;
+            default:
+                System.out.println("this option is not implemented yet.");
+                break;
+        }
+    }
+
+    void switchPractice() {
+        // if else
+
+        // switch
+        int age = 13;
+
+        if (age >= 13 && age <= 19) {
+            System.out.println("you are a teenager");
+        }
+
+        switch (age) {
+            case 13:
+                System.out.println("you are 13");
+                break;
+            case 14:
+                System.out.println("you are 14");
+                break;
+            default:
+                System.out.println("you are not 13 or 14 ");
+        }
+
+        int choice = getChoice();
+        if (choice != 3) {
+            exeMenu(choice);
+            switchPractice();
+        }
+    }
+
+    /**
+     * Conditional statements: if else, booleans, logical opersations
+     * Loops: for, while, do while loop
+     * array: bunch
+     */
+    public static void main(String[] args) {
+        new Gurnoor().switchPractice();
     }
 }
---
+++ Tutoring/src/Calculator/CalcMain.java
@@ -0,0 +1,8 @@
+package Calculator;
+
+public class CalcMain {
+    public static void main(String[] args) {
+        new Calculator().startup();
+
+    }
+}
