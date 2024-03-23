b0ca0a2900a1e2312df54a61ac9aaae7865791be
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2024-03-23 12:08:47 -0400

    23/3

--- Group Tutoring/src/Winter2023/Week11.java
+++ Group Tutoring/src/Winter2023/Week11.java
@@ -14,7 +14,38 @@ public class Week11 {
         System.out.println("hello");
     }
 
+    void printIntArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    /*
+     * populate an array's elements starting form 1 till its length.
+     * ex: arr[10] => 1, 2, 3, 4, 5 ,...., 10
+     */
+    void populateArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+    }
+
     void saturday() {
+        // print numbers form 1 - 10
+        // for (int i = 2; i <= 100; i += 2) {
+        // System.out.print(i + " ");
+        // }
+        // for (int i = 100; i > 0; i--) {
+        // System.out.print(i + " ");
+        // }
+        int n = 12, m, c, d, gkj;
+        int arr[] = new int[100];
+        int arr2[] = { 1, 2, 3, 4, 5, 6, 2, 12, 232 };
+        printIntArray(arr);
+        populateArray(arr);
+        printIntArray(arr);
 
     }
 
@@ -23,6 +54,6 @@ public class Week11 {
     }
 
     public static void main(String[] args) {
-        new Week11().callDay(0);
+        new Week11().callDay(1);
     }
 }
