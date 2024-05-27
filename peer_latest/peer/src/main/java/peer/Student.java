79946270ea09fa0790beb3603a011537ef4096f6
REAPERali00 <alireza2000jadidi@gmail.com>
2024-05-26 22:59:38 -0400

    large update?

---
+++ peer_latest/peer/src/main/java/peer/Student.java
@@ -0,0 +1,13 @@
+package peer;
+
+public class Student {
+  private String name;
+  // - name: String
+  private String courseName;
+  private double gpa;
+
+  public void runner() {
+    System.out.println("Hello World");
+  }
+
+}
---
+++ peer_latest/peer/src/main/java/peer/Trung.java
@@ -0,0 +1,12 @@
+package peer;
+
+public class Trung {
+
+  public void run() {
+    System.out.println(new Hello());
+  }
+
+  public static void main(String[] args) {
+    new Trung().run();
+  }
+}
