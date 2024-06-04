871de72164184de05bcb0b1ecffaf4a88e18e47e
REAPERali00 <alireza2000jadidi@gmail.com>
2024-06-04 08:54:31 -0400

    preps done

---
+++ peer_latest/peer/src/main/java/peer/Amin.java
@@ -0,0 +1,12 @@
+package peer;
+
+/**
+ * Amin
+ */
+public class Amin {
+
+  public void run() {
+    System.out.println("hello world");
+  }
+
+}
--- peer_latest/peer/src/main/java/peer/App.java
+++ peer_latest/peer/src/main/java/peer/App.java
@@ -6,6 +6,6 @@ package peer;
  */
 public class App {
   public static void main(String[] args) {
-    System.out.println(new Hello());
+    new Amin().run();
   }
 }
