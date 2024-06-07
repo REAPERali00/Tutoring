1aa3f147ff47f1b58c29c865bd19e49a70118a7e
REAPERali00 <alireza2000jadidi@gmail.com>
2024-06-07 11:20:24 -0400

    small question fix

--- projects/QuizApplication/src/App.java
+++ projects/QuizApplication/src/App.java
@@ -1,22 +1,8 @@
 public class App {
-    static boolean isLeap(int year) {
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                if (year % 400 == 0) {
-                    return true;
-                }
-                return false;
-            }
-            return true;
-        }
-        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
-    }
 
-    public static void main(String[] args) throws Exception {
-        // Quiz quiz = new Quiz();
-        // quiz.askQuestions();
-        // quiz.quit();
-        System.out.println(0x14 + 012);
-        int reallyLongVarName;
-    }
+  public static void main(String[] args) throws Exception {
+    Quiz quiz = new Quiz();
+    quiz.askQuestions();
+    quiz.quit();
+  }
 }
--- projects/QuizApplication/src/Question.java
+++ projects/QuizApplication/src/Question.java
@@ -1,33 +1,33 @@
 public class Question {
-    private String question;
-    private String[] options;
-    private char correctAnswer;
+  private String question;
+  private String[] options;
+  private char correctAnswer;
 
-    public Question() {
-        this(null, null, ' ');
-    }
+  public Question() {
+    this(null, null, ' ');
+  }
 
-    public Question(String question, String[] options, char correctAnswer) {
-        this.question = question;
-        this.options = options;
-        this.correctAnswer = correctAnswer;
-    }
+  public Question(String question, String[] options, char correctAnswer) {
+    this.question = question;
+    this.options = options;
+    this.correctAnswer = correctAnswer;
+  }
 
-    private String getOptions() {
-        char opt = 'A';
-        StringBuilder sb = new StringBuilder();
-        for (String option : options) {
-            sb.append(String.format("%c. %s\n", opt++, option));
-        }
-        return sb.toString();
+  private String getOptions() {
+    char opt = 'A';
+    StringBuilder sb = new StringBuilder();
+    for (String option : options) {
+      sb.append(String.format("%c. %s\n", opt++, option));
     }
+    return sb.toString();
+  }
 
-    public boolean checkAnswer(char ans) {
-        return this.correctAnswer == ans;
-    }
+  public boolean checkAnswer(char ans) {
+    return this.correctAnswer == ans;
+  }
 
-    public String getQuestion() {
-        return String.format("\n%s \n%s", question, getOptions());
-    }
+  public String getQuestion() {
+    return String.format("\n%s \n%s", question, getOptions());
+  }
 
 }
